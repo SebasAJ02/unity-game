d147912c9473b78f94fcdb43ab7d55750e03dc69
SebasAJ02 <sebastianqdoutlook@gmail.com>
2024-10-22 08:39:20 -0500

    Update Combat Actions.cs

--- Assets/Scripts/Player/Combat Actions.cs
+++ Assets/Scripts/Player/Combat Actions.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CombatActions : MonoBehaviour
 {
-    [SerializeField] private float fireRange;
+    public void Activate_Shield(InputAction.CallbackContext callback)
+    {
+        if(callback.performed)
+        {
+            Debug.Log("Se activo Escudo")
+        }
+    }
 }
